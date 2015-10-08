b3115ba13874948694e17fec03d2133ce2bf9a43
Vin Gardner <vcrgardner@gmail.com>
2015-10-08 12:52:31 +0100

    Adds godeps and readme

--- api/main.go
+++ api/main.go
@@ -1,9 +1,36 @@
 package main
 
-import(
- "fmt"
+import
+(
+  "net/http"
+  "encoding/json"
+  "fmt"
+  "github.com/gorilla/mux"
 )
 
+type API struct {
+  Message string "json:message"
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+  urlParams := mux.Vars(r)
+  name := urlParams["user"]
+  HelloMessage := "Hello, " + name
+
+  message := API{HelloMessage}
+  output, err := json.Marshal(message)
+
+  if err != nil {
+    fmt.Println("Something went wrong!")
+  }
+
+  fmt.Fprintf(w, string(output))
+}
+
 func main() {
- fmt.Println("hello!")
+
+  gorillaRoute := mux.NewRouter()
+  gorillaRoute.HandleFunc("/api/{user:[0-9]+}", Hello)
+  http.Handle("/", gorillaRoute)
+  http.ListenAndServe(":3001", nil)
 }

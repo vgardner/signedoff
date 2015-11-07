5f2a0ce36fdfe85c8610e330d37c8c0861e4b535
Vin Gardner <vcrgardner@gmail.com>
2015-11-07 19:23:40 +0000

    Adds params to releases endpoint

--- website/signedoff/routes/releases.js
+++ website/signedoff/routes/releases.js
@@ -8,9 +8,9 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET users listing. */
-router.get('/vgardner/go-lights', function(req, res, next) {
+router.get('/:user/:repository', function(req, res, next) {
 
-  request("http://localhost:3001/api/releases/vgardner/go-lights", function(error, response, body) {
+  request("http://localhost:3001/api/releases/" + req.params.user + "/" + req.params.repository, function(error, response, body) {
     res.render('index', { title: 'Express', releaseData: body });
    });
 });

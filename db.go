56d62edc2d5d53a9ec0151c32da4c31a1536f96c
Vin Gardner <vcrgardner@gmail.com>
2015-11-15 18:02:52 +0000

    Adds generic interface to object retrieval

--- db.go
+++ db.go
@@ -9,40 +9,37 @@ import (
 	"net/http"
 )
 
+const DBNAME = "signedoff"
+const DBHOST = "localhost:4444"
+
 type Person struct {
 	Name  string
 	Phone string
 }
 
-func saveObject(collectionName string, userName string) {
+func saveObject(collectionName string, object interface{}) {
 	session := getDbSession()
 	defer session.Close()
 
-	collection := session.DB("signedoff").C(collectionName)
-	err := collection.Insert(&Person{userName, "+55 53 8116 9639"})
+	collection := session.DB(DBNAME).C(collectionName)
+	err := collection.Insert(object)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
 func getObject(collectionName string, name string) interface{} {
-	// Start DB session.
-	session, err := mgo.Dial("localhost:4444")
-	if err != nil {
-		panic(err)
-	}
+	session := getDbSession()
 	defer session.Close()
 
-	// Optional. Switch the session to a monotonic behavior.
-	session.SetMode(mgo.Monotonic, true)
+	collection := session.DB(DBNAME).C(collectionName)
 
-	c := session.DB("test").C(collectionName)
-
-	result := Person{}
-	err = c.Find(bson.M{"name": name}).One(&result)
+	var result interface{}
+	err := collection.Find(bson.M{"name": name}).One(&result)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	return result
 }
 
@@ -50,51 +47,21 @@ func dbTest(w http.ResponseWriter, r *http.Request) {
 	urlParams := mux.Vars(r)
 	userName := urlParams["user"]
 
-	saveObject("people", userName)
+	saveObject("people", &Person{userName, "+55 53 8116 9639"})
 
-	//result := getObject("people", userName)
+	result := getObject("people", userName)
 
-	json.NewEncoder(w).Encode("saved")
+	json.NewEncoder(w).Encode(result)
 }
 
 func getDbSession() *mgo.Session {
 	// Start session.
-	session, err := mgo.Dial("localhost:4444")
+	session, err := mgo.Dial(DBHOST)
 	if err != nil {
 		panic(err)
 	}
-
 	// Optional. Switch the session to a monotonic behavior.
 	session.SetMode(mgo.Monotonic, true)
 
 	return session
-}
-
-func dbTestx(w http.ResponseWriter, r *http.Request) {
-	urlParams := mux.Vars(r)
-	userName := urlParams["user"]
-
-	session, err := mgo.Dial("localhost:4444")
-	if err != nil {
-		panic(err)
-	}
-	defer session.Close()
-
-	// Optional. Switch the session to a monotonic behavior.
-	session.SetMode(mgo.Monotonic, true)
-
-	c := session.DB("test").C("people")
-	err = c.Insert(&Person{userName, "+55 53 8116 9639"},
-		&Person{"Cla", "+55 53 8402 8510"})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	result := Person{}
-	err = c.Find(bson.M{"name": userName}).One(&result)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	json.NewEncoder(w).Encode(result)
 }
